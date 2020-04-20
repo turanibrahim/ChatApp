fc0e8b121379c35eb9a7f4b55fcbc6c5d37724c6
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-04-20 20:36:25 +0300

    some depens added to project and made that depens global settings

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
+import VueCookies from 'vue-cookies'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+Vue.use(VueCookies)
 
-new Vue({
+// set expire time 7day
+Vue.$cookies.config('7d')
+
+var vm = new Vue({
+  el: "#app",
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+global.vm = vm;
