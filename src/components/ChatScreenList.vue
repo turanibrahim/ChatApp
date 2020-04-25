ea862514e11e7f004216e6750c91fc83169ada9e
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-04-25 06:55:12 +0300

    conversation list component added

---
+++ src/components/ChatScreenList.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-list-item 
+    link
+    avatar
+  >
+    <v-list-item-avatar>
+      <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
+    </v-list-item-avatar>
+    <v-list-item-content >
+      <v-list-item-title>
+        {{ name }}
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props:{
+    name: String,
+  },
+  methods:{
+  }
+}
+</script>
+
+<style>
+
+</style>
