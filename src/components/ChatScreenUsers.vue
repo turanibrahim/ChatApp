40d7ef39db7cfcc2c1416fe5a15e8d61081a8aa1
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-04-25 06:56:54 +0300

    member list component added

---
+++ src/components/ChatScreenUsers.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-list-item
+    link
+    avatar
+  >
+    <v-list-item-avatar>
+      <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
+    </v-list-item-avatar>
+    <v-list-item-content >
+      <v-list-item-title>
+        <v-badge
+          :color="(isOnline) ? 'green' : 'red'"
+          :content="(isOnline)? 'Online': 'Offline'"
+          class="font-weight-bold"
+        >
+          {{ username }}
+        </v-badge>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props:{
+    username: String,
+    isOnline: Boolean,
+  },
+}
+</script>
+
+<style>
+
+</style>
