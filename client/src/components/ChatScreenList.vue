74d0aef4eb1ad70d3b1ae805f0ba6d3ccecddef7
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-04-27 09:28:18 +0300

    integrating backend and frontend

---
+++ client/src/components/ChatScreenList.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-list-item
+    link
+    avatar
+  >
+    <template v-if="!isRoom">
+      <v-list-item-avatar>
+        <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
+      </v-list-item-avatar>
+    </template>
+    <v-list-item-content >
+      <v-list-item-title>
+        <template v-if="!isRoom">
+          <v-badge
+            :color="(isOnline) ? 'green' : 'red'"
+            :content="(isOnline)? 'Online': 'Offline'"
+            class="font-weight-bold"
+            v-if="username"
+          >
+            {{ (username) ? username : '' }}
+          </v-badge>
+        </template>
+        <span class="font-weight-bold">{{ (name) ? name : '' }}</span>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props:{
+    username: String,
+    isOnline: Boolean,
+    isRoom: Boolean,
+    name: String,
+  },
+}
+</script>
+
+<style>
+
+</style>
